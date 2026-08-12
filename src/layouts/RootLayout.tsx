27b93b1858e9c00ff8693732a292b81ce89a1255
yuan <yuanchavez274@gmail.com>
2026-08-13 07:47:16 +0800

    feat: add ScrollRestoration to RootLayout for improved navigation state management

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,9 +1,14 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, ScrollRestoration } from 'react-router-dom'
 
 /**
  * Shared application shell. Add global nav/footer here as
  * student- and company-facing chrome is introduced in later sprints.
  */
 export function RootLayout() {
-  return <Outlet />
+  return (
+    <>
+      <Outlet />
+      <ScrollRestoration />
+    </>
+  )
 }
